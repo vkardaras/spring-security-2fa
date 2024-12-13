676d174d05abbe1532385d41b3a3a1b7f4442eb3
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-12-13 14:01:55 +0200

    initialize data

--- src/main/java/com/vasiliskardaras/security_jpa/config/InitializeData.java
+++ src/main/java/com/vasiliskardaras/security_jpa/config/InitializeData.java
@@ -16,7 +16,8 @@ import java.util.Set;
 public class InitializeData {
 
     @Bean
-    CommandLineRunner commandLineRunner(UserRepository userRepository, PasswordEncoder encoder, AuthorityRepository authorityRepository) {
+    CommandLineRunner commandLineRunner(UserRepository userRepository, PasswordEncoder encoder,
+            AuthorityRepository authorityRepository) {
         return args -> {
             if (authorityRepository.findAll().isEmpty()) {
                 Authority authUser = authorityRepository.save(new Authority("ROLE_USER"));
@@ -25,7 +26,8 @@ public class InitializeData {
                 if (userRepository.findAll().isEmpty()) {
 
                     userRepository.save(new User("user", encoder.encode("password"), "ROLE_USER", Set.of(authUser)));
-                    userRepository.save(new User("admin", encoder.encode("password"), "ROLE_USER,ROLE_ADMIN", Set.of(authUser, authAdmin)));
+                    userRepository.save(new User("admin", encoder.encode("password"), "ROLE_USER,ROLE_ADMIN",
+                            Set.of(authUser, authAdmin)));
                 }
             }
         };
